b841690321b9c51915153c5837117f79357f1460
masuri <psh0258@gmail.com>
2024-08-03 00:24:38 +0900

    feat: 설정에 address 추가

--- NolowaNetwork/Models/Configuration/NetworkConfigurationModel.cs
+++ NolowaNetwork/Models/Configuration/NetworkConfigurationModel.cs
@@ -1,7 +1,8 @@
-﻿namespace NolowaNetwork.Models.Configuration
+namespace NolowaNetwork.Models.Configuration
 {
     public class NetworkConfigurationModel
     {
+        public string Address { get; set; } = string.Empty;
         public string ServerName { get; set; } = string.Empty;
         public string HostName { get; set; } = string.Empty;
         public string ExchangeName { get; set; } = string.Empty;
